801da4751af1af858388c0fe22a8c714207bc3c2
aspotdot <20707146+aspotdot@users.noreply.github.com>
2025-02-20 21:52:43 -0500

    3d: encoder map

--- keyboards/handwired/3dcx_macro/keymaps/default/keymap.c
+++ keyboards/handwired/3dcx_macro/keymaps/default/keymap.c
@@ -103,8 +103,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_00] =  { ENCODER_CCW_CW(KC_UP,   KC_DOWN) },
-    [_MO] =  { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
+    [_00] =  { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
+    [_MO] =  { ENCODER_CCW_CW(KC_DOWN, KC_UP) },
+    [_FN] =  { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
     [_XL] =  { ENCODER_CCW_CW(KC_LEFT, KC_RGHT) },
 };
 #endif
